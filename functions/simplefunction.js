9a130155fc5dac8ccf04f152b221bf4e7cc92f38
shubham <shubham30898@gmail.com>
2023-09-27 23:00:28 +0530

    Learned about functions basics

---
+++ functions/simplefunction.js
@@ -0,0 +1,16 @@
+function sum(a,b){
+    let result = a+b;
+    console.log("sum of ab and b:",result);
+}
+
+let a = 10;
+let b = 20;
+sum(a,b)
+
+function subtract(a,b){
+    return a-b;
+}
+
+let a1 = 10;
+let b1 = 20;
+console.log("result is:",subtract(b1,a1));
